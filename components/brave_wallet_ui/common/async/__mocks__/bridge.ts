3ecd9577a49c79cb43ba19e0556768428e71e4c3
Josh Leonard <jleonard@brave.com>
2022-03-08 22:31:23 -0700

    chore: refactor WalletApiProxy mock

--- components/brave_wallet_ui/common/async/__mocks__/bridge.ts
+++ components/brave_wallet_ui/common/async/__mocks__/bridge.ts
@@ -1,59 +1,60 @@
 import { BraveWallet } from '../../../constants/types'
 import WalletApiProxy from '../../wallet_api_proxy'
+export class MockedWalletApiProxy {
+    mockQuote = {
+        allowanceTarget: '',
+        price: '',
+        guaranteedPrice: '',
+        to: '',
+        data: '',
+        value: '',
+        gas: '0',
+        estimatedGas: '0',
+        gasPrice: '0',
+        protocolFee: '0',
+        minimumProtocolFee: '0',
+        buyTokenAddress: '',
+        sellTokenAddress: '',
+        buyAmount: '0',
+        sellAmount: '0',
+        sellTokenToEthRate: '1',
+        buyTokenToEthRate: '1'
+    }
 
-const mockQuote = {
-    allowanceTarget: '',
-    price: '',
-    guaranteedPrice: '',
-    to: '',
-    data: '',
-    value: '',
-    gas: '0',
-    estimatedGas: '0',
-    gasPrice: '0',
-    protocolFee: '0',
-    minimumProtocolFee: '0',
-    buyTokenAddress: '',
-    sellTokenAddress: '',
-    buyAmount: '0',
-    sellAmount: '0',
-    sellTokenToEthRate: '1',
-    buyTokenToEthRate: '1'
-} as BraveWallet.SwapResponse
+    swapService = {
+        getTransactionPayload: async () => ({
+            success: true,
+            errorResponse: {},
+            response: this.mockQuote
+        }),
+        getPriceQuote: async () => ({
+            success: true,
+            errorResponse: {},
+            response: {}
+        })
+    }
 
-export default function getAPIProxy (): WalletApiProxy {
-    return {
-        swapService: {
-            getTransactionPayload: async () => ({
-                success: true,
-                errorResponse: {},
-                response: {
-                    ...mockQuote,
-                    gasPrice: '10',
-                    gas: '100000',
-                    sellAmount: '10000000000000000000' // 10 BAT
-                }
-            }),
-            getPriceQuote: async () => ({
-                success: true,
-                errorResponse: {},
-                response: {}
-            })
-        },
-        ethTxManagerProxy: {
-            getGasEstimation1559: async () => {
-                return {
-                    estimation: {
-                        slowMaxPriorityFeePerGas: '0',
-                        slowMaxFeePerGas: '0',
-                        avgMaxPriorityFeePerGas: '0',
-                        avgMaxFeePerGas: '0',
-                        fastMaxPriorityFeePerGas: '0',
-                        fastMaxFeePerGas: '0',
-                        baseFeePerGas: '0'
-                    } as BraveWallet.GasEstimation1559 | null
-                }
+    ethTxManagerProxy = {
+        getGasEstimation1559: async () => {
+            return {
+                estimation: {
+                    slowMaxPriorityFeePerGas: '0',
+                    slowMaxFeePerGas: '0',
+                    avgMaxPriorityFeePerGas: '0',
+                    avgMaxFeePerGas: '0',
+                    fastMaxPriorityFeePerGas: '0',
+                    fastMaxFeePerGas: '0',
+                    baseFeePerGas: '0'
+                } as BraveWallet.GasEstimation1559 | null
             }
         }
-    } as any
+    }
+
+    setMockedQuote (newQuote: typeof this.mockQuote) {
+        this.mockQuote = newQuote
+    }
+}
+
+export default function getAPIProxy (): Partial<WalletApiProxy> {
+    return new MockedWalletApiProxy() as unknown as Partial<WalletApiProxy> & MockedWalletApiProxy
 }
