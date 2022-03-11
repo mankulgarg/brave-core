ab1ad66f962722fd072fd9cd7f71893e340fc6dc
Mario Sanchez Prada <mario@igalia.com>
2022-03-11 12:49:49 +0100

    fixup! Migrate away from GetListDeprecated/TakeListDeprecated
    Fix npm run lint issues

--- vendor/bat-native-ledger/src/bat/ledger/internal/credentials/credentials_util.h
+++ vendor/bat-native-ledger/src/bat/ledger/internal/credentials/credentials_util.h
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVELEDGER_CREDENTIALS_CREDENTIALS_UTIL_H_
-#define BRAVELEDGER_CREDENTIALS_CREDENTIALS_UTIL_H_
+#ifndef BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_CREDENTIALS_CREDENTIALS_UTIL_H_
+#define BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_CREDENTIALS_CREDENTIALS_UTIL_H_
 
 #include <memory>
 #include <string>
@@ -66,4 +66,4 @@ bool GenerateSuggestionMock(
 }  // namespace credential
 }  // namespace ledger
 
-#endif  // BRAVELEDGER_CREDENTIALS_CREDENTIALS_UTIL_H_
+#endif  // BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_CREDENTIALS_CREDENTIALS_UTIL_H_
