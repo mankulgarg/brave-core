e641a20daae241cc5a34d903eedbfeb8ca0b2049
sergey <spylogsster@gmail.com>
2022-03-12 17:06:25 +0300

    Parse Uint64 in JSON with third_party/jsoncpp

--- components/brave_wallet/browser/fil_response_parser.cc
+++ components/brave_wallet/browser/fil_response_parser.cc
@@ -15,6 +15,8 @@
 #include "brave/components/brave_wallet/browser/json_rpc_response_parser.h"
 #include "brave/components/brave_wallet/common/brave_wallet_types.h"
 #include "brave/components/brave_wallet/common/hex_utils.h"
+#include "third_party/jsoncpp/source/include/json/reader.h"
+#include "third_party/jsoncpp/source/include/json/value.h"
 
 namespace brave_wallet {
 
@@ -23,14 +25,26 @@ bool ParseFilGetBalance(const std::string& json, std::string* balance) {
 }
 
 bool ParseFilGetTransactionCount(const std::string& json, uint64_t* count) {
+  // Default base::JSONReader is unable to parse uint64 values.
+  // Taking third_party/jsoncpp to parse payload in this case because
+  // https://github.com/filecoin-project/lotus/blob/master/chain/types/message.go#L36
+  // the nonce has uint64 type.
   base::Value result;
-  if (!ParseResult(json, &result))
+  Json::CharReaderBuilder builder;
+  Json::CharReaderBuilder::strictMode(&builder.settings_);
+  std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+  Json::Value root_node;
+  std::string error_msg;
+  const bool succeeded = reader->parse(json.data(), json.data() + json.length(),
+                                       &root_node, &error_msg);
+  if (!succeeded || !root_node) {
     return false;
-  if (result.is_int() || result.is_double()) {
-    *count = result.GetDouble();
-    return true;
   }
-  return false;
+  if (!root_node["result"] || !root_node["result"].isUInt64()) {
+    return false;
+  }
+  *count = root_node["result"].asUInt64();
+  return true;
 }
 
 }  // namespace brave_wallet
